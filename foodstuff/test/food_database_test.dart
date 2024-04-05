164876db5e5ca5884bc3a3d0bf0603bf298d45f1
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2024-04-05 15:10:52 +0200

    add test for food database

---
+++ foodstuff/test/food_database_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:foodstuff/food_database.dart';
+import 'package:mockito/mockito.dart';
+
+class MockPathProviderPlatform extends Mock
+    with MockPlatformInterfaceMixin
+    implements PathProviderPlatform {
+      @override
+        Future<String> getApplicationDocumentsPath() async {
+          final Uri basedir = (goldenFileComparator as LocalFileComparator).basedir;
+          var directoryPath = basedir.toFilePath();
+          return "${directoryPath}test_data";
+      }
+    }
+
+void main() {
+
+  setUp(() async {
+      PathProviderPlatform.instance = MockPathProviderPlatform();
+    });
+
+  test('Can create a food database', () {
+    var futureDatabase = FoodDatabase.create();
+    expect(futureDatabase, isA<Future<FoodDatabase>>());
+  });
+}
